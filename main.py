e36f819b61068c76cb7d8dfc8c52464be65e2075
tridielas <eoin.nelson2@gmail.com>
2025-03-22 12:27:25 -0400

    the box is working

--- main.py
+++ main.py
@@ -5,9 +5,16 @@ import pygame
 from constants import *
 
 def main():
+    pygame.init()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    display.flip()
     
 if __name__ == "__main__":
     main()
